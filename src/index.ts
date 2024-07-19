16a4a2330853b79eba3e4a422c26e812f572a57c
yocopk <yocopk@gmail.com>
2024-07-19 16:34:06 +0200

    general fix

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,83 @@
 // #region ::: IMPORTS :::
-import { createClient } from "@vercel/postgres";
 import express, { Request, Response } from "express";
-import { config } from "dotenv";
-import { Server } from "socket.io";
+import { createClient } from '@vercel/postgres';
+import { config } from 'dotenv';
 import { createServer } from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 import path from "path";
+// #endregion
 
-config();
-
-const PORT = process.env.PORT || 3000;
+// #region ::: CONFIGURATIONS :::
+config(); // carica le variabili d'ambiente
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 const server = createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
 const client = createClient({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL
 });
+const io = new Server(server, { cors: { origin: "*",
+    methods: ["GET", "POST"],
+ } });
 
-client.connect();
-
-app.use(
-  cors({
+app.use(cors({ 
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-  express.json()
-);
+})); // middleware per il parsing del JSON
+// #endregion
+
+// Connect to the database
+(async () => {
+    try {
+        await client.connect();
+        console.log("Connected to the database");
+    } catch (error) {
+        console.error("Failed to connect to the database", error);
+    }
+})();
 
 io.on("connection", (socket) => {
-  socket.on("message-sent", (message) => {
-    client.query(
-      `INSERT INTO messages (content) VALUES ($1)`,
-      [message],
-      (error) => {
-        if (!error) io.emit("message-received", message);
-      }
-    );
-  });
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
+    console.log("a user connected");
+
+    socket.on("message-sent", (msg) => {
+        client.query("INSERT INTO messages (content) VALUES ($1)", [msg], (err) => {
+            if (!err) io.emit("message-received", msg);;
+        });
+    })
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        
+    })
+})
 
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+    res.sendFile(path.join(__dirname, "index.html"));
+})
 
-app.get("/api/messages", (req: Request, res: Response) => {
-  client.query("SELECT * FROM messages", (error, response) => {
-    if (error) res.status(500).json({ error });
-    else res.status(200).json(response.rows);
-  });
-});
-
-app.post("/api/messages", (req: Request, res: Response) => {
-  const { content } = req.body;
-  client.query(
-    `INSERT INTO messages (content) VALUES ($1)`,
-    [content],
-    (error) => {
-      if (error) res.status(500).json({ error });
-      else res.status(200).json({ message: "Message created successfully" });
+// Define routes
+app.get("/api/messages", async (req: Request, res: Response) => { // restituisce tutti i messaggi
+    try {
+        const response = await client.query('SELECT * FROM messages');
+        return res.status(200).json(response.rows);
+    } catch (error) {
+        return res.status(500).json({ message: "connection error", error });
     }
-  );
 });
 
+app.post("/api/messages", async (req: Request, res: Response) => {
+    try {
+        const { content } = req.body;
+        await client.query('INSERT INTO messages (content) VALUES ($1)', [content]);
+        return res.status(200).json({ message: "message created" });
+    } catch (error) {
+        return res.status(500).json({ message: "connection error", error });
+    }
+});
+
+// Start the server
 server.listen(PORT, () => {
-  console.log(`Server API is running http://localhost:${PORT}`);
+    console.log(`Server started at http://localhost:${PORT}`);
 });
